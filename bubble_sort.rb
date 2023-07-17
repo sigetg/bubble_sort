aa4f4986f03b1df9c3171bd9f08da3c7b62a744a
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-07-17 15:56:34 -0400

    complete bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,6 +3,7 @@
 def bubble_sort(num_array)
   index = 0
   while index < num_array.length - 1
+    # if a switch is made, the index is reset to 0
     if num_array[index] > num_array[index+1]
       num_array[index], num_array[index+1] = num_array[index+1], num_array[index]
       index = 0
