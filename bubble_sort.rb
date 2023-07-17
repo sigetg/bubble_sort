a19f7e4da72befee68eef399d9e9c694410165c4
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-07-17 15:55:21 -0400

    Complete bubble_sort method

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+#Author: George Sigety
+
+def bubble_sort(num_array)
+  index = 0
+  while index < num_array.length - 1
+    if num_array[index] > num_array[index+1]
+      num_array[index], num_array[index+1] = num_array[index+1], num_array[index]
+      index = 0
+      next
+    end
+    index += 1
+  end
+  num_array
+end
+
+puts bubble_sort([4,3,78,2,0,2])
+
